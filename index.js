6856d062a313c78df6f780b5000d0e2aa5b8b941
techay-mihir-simform <techaypatel123@gmail.com>
2022-09-24 12:01:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express')
+require("dotenv").config();
+
+var app = express()
+
+app.set('port', (process.env.PORT))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+
+app.get('/env', function(request, response) {
+  response.send(`ENv is : ${process.env.ENV}`)
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
